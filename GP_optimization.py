8358c2bd807e429146026c3b6f97c3bb0f16c127
Feng Yao <46351482+feng-y16@users.noreply.github.com>
2019-02-22 15:12:54 +0800

    Add files via upload

--- GP_optimization.py
+++ GP_optimization.py
@@ -2,31 +2,40 @@ from __future__ import print_function
 import numpy as np
 import random
 import math
-from sklearn.gaussian_process.kernels import Matern
 from GP_utils import *
 
 class STABLEOPT:
-    def __init__(self,beta,init_num,mu0,epsilon,D,iter=100):
+    def __init__(self,beta,init_num,mu0,epsilon,D,iter=100,init_point_option="function value weighted"):
         random.seed(10)
         self.theta0=1
         self.thetai=np.ones(D)
         self.sigma2=1
         self.mu0=mu0
-        self.init_num=init_num
-        self.iter=iter
+        self.init_num=init_num#初始采样点的数目
+        self.iter=iter#maxmin问题的迭代次数
         self.t=0
         self.beta=beta
-        self.epsilon=epsilon
-        self.T=len(beta)
-        self.D=D
+        self.epsilon=epsilon#delta与x的距离上限
+        self.T=len(beta)#beta的长度就是总数据量（=初始采样点数+贝叶斯优化点数）
+        self.D=D#维数
         self.x=np.zeros((self.T,self.D))
         self.y=np.zeros(self.T)
-        self.results=-10000*np.ones(self.T)
-        self.bound=self.epsilon*np.ones((D,2))
+        self.results=-10000*np.ones(self.T)#函数值
+        self.bound=self.epsilon*np.ones((D,2))#delta的每一维的上下界（无穷范数意义下）
         for i in range(0,D):
             self.bound[i][0]=-self.epsilon
+        self.iter_initial_point=np.zeros(D)#第一次迭代的起始点
+        if init_point_option=="best point":#过去最优点开始迭代
+            self.init_point_option=1
+            return
+        if init_point_option=="random sample point":#随机选择过去采样点开始迭代
+            self.init_point_option=2
+            return
+        if init_point_option=="function value weighted":#按照过去采样点函数值加权（线性加权）
+            self.init_point_option=3
+            return
 
-    def k(self,x1,x2):
+    def k(self,x1,x2):#核函数
         r=0
         for i in range(0,self.D):
             r=r+(x1-x2)[i]**2/self.thetai[i]**2
@@ -34,7 +43,7 @@ class STABLEOPT:
         return self.theta0**2*math.exp(-math.sqrt(5)*r)*(1+math.sqrt(5)*r+5/3*r**2)
         #return self.theta0**2*math.exp(-r)
 
-    def k2(self,x1,x2,theta0,thetai):
+    def k2(self,x1,x2,theta0,thetai):#带参数的核函数
         r=0
         for i in range(0,self.D):
             r=r+(x1-x2)[i]**2/thetai[i]**2
@@ -42,7 +51,7 @@ class STABLEOPT:
         return theta0**2*math.exp(-math.sqrt(5)*r)*(1+math.sqrt(5)*r+5/3*r**2)
         #return self.theta0**2*math.exp(-r)
 
-    def dis_fun(self,x1,x2):
+    def dis_fun(self,x1,x2):#距离函数（无穷范数）
         return np.linalg.norm(x1-x2, ord=np.inf)
 
     def k_t(self,x):
@@ -75,17 +84,17 @@ class STABLEOPT:
                 K[i][j]=self.k2(self.x[i],self.x[j],theta0,thetai)
         return K
 
-    def get_value(self,x):
+    def get_value(self,x):#优化目标函数，带高斯噪声
         y=100.0-np.linalg.norm(x)**2-np.linalg.norm(x-np.ones(self.D))**2+np.random.normal(0,0.1)#here we can modify y=f(x)
         return y
 
-    def observe(self,x):#add selected data
+    def observe(self,x):#观测指定点的函数值
         self.x[self.t]=x
         self.y[self.t]=self.get_value(x)
         self.t=self.t+1
         return self.y[self.t-1]
 
-    def init(self):#feed initial data
+    def init(self):#初始添加观测数据
         x=random.uniform(-30,30)
         y=random.uniform(-30,30)
         z=random.uniform(-30,30)
@@ -98,7 +107,7 @@ class STABLEOPT:
         self.t=self.t+1
         return 0
 
-    def get_prior(self):
+    def get_prior(self):#获得先验
         m=np.mean(self.y[range(0,self.t)])
         def log_likehood(x):
             theta0=x[0]
@@ -117,7 +126,7 @@ class STABLEOPT:
         for i in range(0,self.D+3):
             bound[i][0]=-1000000
             bound[i][1]=1000000
-        bound[self.D+2][0]=0
+        bound[self.D+2][0]=0#优化区域。实际上除了sigma平方大于0，无别的限制
         x_opt=ZOSGD_bounded(log_likehood,np.ones(self.D+3),bound,1,0.03,100)
         self.theta0=x_opt[0]
         self.thetai=x_opt[range(1,self.D+1)]
@@ -125,7 +134,7 @@ class STABLEOPT:
         self.sigma2=x_opt[self.D+2]
         return 0
 
-    def pred(self,x):
+    def pred(self,x):#预测指定x处的mu
         shape_x=np.shape(x)
         mu=np.zeros(shape_x[0])
         for i in range(0,shape_x[0]):
@@ -135,10 +144,29 @@ class STABLEOPT:
                 print("Singular matrix when computing pred. Small identity added.")
                 inv=np.linalg.inv(self.K_t()+self.sigma2*np.identity(self.t)+0.01*np.identity(self.t))
              finally:
-                mu[i]=self.mu0+((np.array(self.k_t(x[i])).T).dot(inv)).dot(self.y[0:self.t])
+                mu[i]=self.mu0+((np.array(self.k_t(x[i])).T).dot(inv)).dot(self.y[0:self.t]-np.mean(self.y[0:self.t]))
                 return mu
+    
+    def select_init_point(self):
+        if self.init_point_option==1:
+            index=np.where(self.results==np.max(self.results))
+            return self.x[index][0]
+        if self.init_point_option==2:
+            index=random.randint(0,t-1)
+            return self.x[index]
+        if self.init_point_option==3:
+            results=self.results[range(0,self.t)]
+            results=results+np.min(results)
+            results=results/np.sum(results)
+            p=random.uniform(0,1)
+            temp=0
+            for i in range(0,self.t):
+                temp=temp+results[i]
+                if temp>p:
+                    return self.x[i]
+            print("Function value weighted method error!")
 
-    def run_onestep(self,iter):
+    def run_onestep(self,iter):#寻找给定先验下下一个最优采样点
         if self.t<self.T:
             def ucb(x):
                 try:
@@ -170,7 +198,7 @@ class STABLEOPT:
                         print("Let sigma2=0")
                         sigma_square=0
                 return mu-np.sqrt(self.beta[self.t]*sigma_square)
-            x=np.zeros(self.D)
+            x=self.select_init_point()
             delta=np.zeros(self.D)
             for i in range(0,iter):
                 x=ZOSGA(ucb,x,0.3,0.05,2)-delta
@@ -179,14 +207,14 @@ class STABLEOPT:
             self.x[self.t]=x+delta
             #while not (np.unique(self.x[range(0,self.t+1)])==self.x[range(0,self.t+1)]):
             #    self.x[self.t]=self.x[self.t]+np.random.multivariate_normal(np.zeros(self.D),0.01*np.identity(self.D))
-            print("selected x+delta=",end="")
+            print("Selected x+delta=",end="")
             print(x+delta)
-            max_value=self.observe(self.x[self.t])
-            print("function value=",end="")
-            print(max_value)
-            self.results[self.t-1]=max_value#note that t has increased
+            fun_value=self.observe(self.x[self.t])
+            print("Function value=",end="")
+            print(fun_value)
+            self.results[self.t-1]=fun_value#这里t在self.observe()已经更新为现有的点的个数，从而需要-1
         else:
-            print("t error!")
+            print("t value error!")
             return 0
 
     def run(self):
@@ -197,7 +225,7 @@ class STABLEOPT:
             print("##################################################################")
             print(i+1,end="")
             print("/",end="")
-            print(self.T-self.init_num)
+            print(self.T-self.init_num)#需要通过贝叶斯方法采样的点的数目
             print("Getting prior……")
             #pred=self.pred(self.x[range(0,self.t)])
             self.get_prior()
--- GP_utils.py
+++ GP_utils.py
@@ -1,10 +1,8 @@
 from __future__ import print_function
 import numpy as np
 import random
-import msvcrt
-import sys
 
-def ZOSGD(f,x0,step,lr=0.1,iter=100,Q=5):
+def ZOSGD(f,x0,step,lr=0.1,iter=100,Q=5):#x0：迭代起始点，step：计算梯度所用步长，lr：学习率，iter：迭代次数，Q：每次梯度计算采样次数
     D=len(x0)
     x_opt=x0
     best_f=f(x0)
@@ -31,15 +29,15 @@ def ZOSGD(f,x0,step,lr=0.1,iter=100,Q=5):
             x_opt=x_temp
         else:
             flag=flag+1
-            if flag%2==0:
+            if flag%2==0:#多次效果波动，则减小步长和学习率
                 step=step*0.75
                 lr=lr*0.75
             p=2**(-i-1)
-            if np.random.uniform()<p:
+            if np.random.uniform()<p:#如果优化结果变差，也有一定概率接受
                 x_opt=x_temp
     return x_opt
 
-def ZOSGA(f,x0,step,lr=0.1,iter=100,Q=5):
+def ZOSGA(f,x0,step,lr=0.1,iter=100,Q=5):#x0：迭代起始点，step：计算梯度所用步长，lr：学习率，iter：迭代次数，Q：每次梯度计算采样次数
     D=len(x0)
     x_opt=x0
     best_f=f(x0)
@@ -66,16 +64,16 @@ def ZOSGA(f,x0,step,lr=0.1,iter=100,Q=5):
             x_opt=x_temp
         else:
             flag=flag+1
-            if flag%2==0:
+            if flag%2==0:#多次效果波动，则减小步长和学习率
                 step=step*0.75
                 lr=lr*0.75
             p=2**(-i-1)
-            if np.random.uniform()<p:
+            if np.random.uniform()<p:#如果优化结果变差，也有一定概率接受
                 x_opt=x_temp
     return x_opt
 
 
-def ZOSGD_bounded(f,x0,bound,step,lr=0.1,iter=100,Q=5):
+def ZOSGD_bounded(f,x0,bound,step,lr=0.1,iter=100,Q=5):#x0：迭代起始点，bound：每一维的上下界，step：计算梯度所用步长，lr：学习率，iter：迭代次数，Q：每次梯度计算采样次数
     D=len(x0)
     x_opt=x0
     best_f=f(x0)
@@ -94,7 +92,7 @@ def ZOSGD_bounded(f,x0,bound,step,lr=0.1,iter=100,Q=5):
             if (x_opt[j]+dx[j]<bound[j][0]) or (x_opt[j]+dx[j]>bound[j][1]):
                 flag2=1
                 break
-        if flag2==1:
+        if flag2==1:#越界则减小步长和学习率
             lr=lr*0.8
             step=step*0.8
             continue
@@ -111,15 +109,15 @@ def ZOSGD_bounded(f,x0,bound,step,lr=0.1,iter=100,Q=5):
             x_opt=x_temp
         else:
             flag1=flag1+1
-            if flag1%3==0:
-                step=step*0.95
-                lr=lr*0.95
+            if flag1%3==0:#多次效果波动，则减小步长和学习率
+                step=step*0.85
+                lr=lr*0.85
             p=2**(-i-1)
-            if np.random.uniform()<p:
+            if np.random.uniform()<p:#如果优化结果变差，也有一定概率接受
                 x_opt=x_temp
     return x_opt
 
-def ZOSGA_bounded(f,x0,bound,step,lr=0.1,iter=100,Q=5):
+def ZOSGA_bounded(f,x0,bound,step,lr=0.1,iter=100,Q=5):#x0：迭代起始点，bound：每一维的上下界，step：计算梯度所用步长，lr：学习率，iter：迭代次数，Q：每次梯度计算采样次数
     D=len(x0)
     x_opt=x0
     best_f=f(x0)
@@ -138,7 +136,7 @@ def ZOSGA_bounded(f,x0,bound,step,lr=0.1,iter=100,Q=5):
             if (x_opt[j]+dx[j]<bound[j][0]) or (x_opt[j]+dx[j]>bound[j][1]):
                 flag2=1
                 break
-        if flag2==1:
+        if flag2==1:#越界则减小步长和学习率
             lr=lr*0.8
             step=step*0.8
             continue
@@ -155,11 +153,11 @@ def ZOSGA_bounded(f,x0,bound,step,lr=0.1,iter=100,Q=5):
             x_opt=x_temp
         else:
             flag1=flag1+1
-            if flag1%3==0:
-                step=step*0.95
-                lr=lr*0.95
+            if flag1%3==0:#如果优化结果变差，也有一定概率接受
+                step=step*0.85
+                lr=lr*0.85
             p=2**(-i-1)
-            if np.random.uniform()<p:
+            if np.random.uniform()<p:#如果优化结果变差，也有一定概率接受
                 x_opt=x_temp
     return x_opt
 
